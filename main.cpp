ccd3302412f6354d889ffb4d80e71bb8fcbf4f1e
Mohsen Poureiny <mohsenpoureiny@outlook.com>
2021-12-06 09:33:43 +0330

    write in C++

--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
+ long int n, t, max;
+
+ cout << "Enter a number :";
+
+ cin >> n;
+ vector<int> numbers = {};
+ while (n != 0)
+ {
+  t = n % 10;
+  numbers.push_back(t);
+  n = n / 10;
+ }
+
+ for (int e : numbers)
+ {
+  if (max < e)
+  {
+   max = e;
+  }
+ }
+ cout << max << endl;
 
  return 0;
 }
